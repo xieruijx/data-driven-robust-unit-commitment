412b59477a84d7af470c5c203852c57a5cdfb7b4
Rui Xie <xie_rui1996@163.com>
2024-06-04 17:32:14 +0800

    Organize class case

--- src/utils/case.py
+++ src/utils/case.py
@@ -6,79 +6,6 @@ class Case(object):
     """
     Case class for power system cases
     """
-
-    @staticmethod
-    def process_case(mpc):
-        """
-        Fetch and calculate some parameters
-        """
-        mpc['branch'] = mpc['branch'][mpc['branch'][:, 10] == 1] # Branches that are on
-
-        mpc['n_b'] = mpc['bus'].shape[0] # Number of buses
-        mpc['n_l'] = mpc['branch'].shape[0] # Number of branches
-        mpc['n_g'] = mpc['gen'].shape[0] # Number of generators
-
-        mpc['c_x_pg'] = mpc['gencost'][:, 5] * mpc['baseMVA'] # Day-ahead power cost coefficient
-        mpc['c_x_rp'] = mpc['gencost'][:, 7] * mpc['baseMVA'] # Up reserve cost coefficient
-        mpc['c_x_rn'] = mpc['gencost'][:, 8] * mpc['baseMVA'] # Down reserve cost coefficient
-        mpc['c_x_og'] = np.zeros((mpc['n_g'],)) # On cost
-        mpc['c_x_ou'] = mpc['gencost'][:, 1] # Turn on cost
-        mpc['c_x_od'] = mpc['gencost'][:, 2] # Shut down cost
-        mpc['c_y_rp'] = mpc['gencost'][:, 9] * mpc['baseMVA'] # Real-time up cost coefficient
-        mpc['c_y_rn'] = mpc['gencost'][:, 10] * mpc['baseMVA'] # Real-time down cost coefficient
-
-        # Bus-branch index matrix
-        mpc['bus_branch'] = np.zeros((mpc['n_b'], mpc['n_l'])) # 1: flow out; -1: flow in
-        for branch in range(mpc['n_l']):
-            mpc['bus_branch'][int(mpc['branch'][branch, 0] - 1), branch] = 1
-            mpc['bus_branch'][int(mpc['branch'][branch, 1] - 1), branch] = -1
-
-        # Bus-generator index matrix
-        mpc['bus_gen'] = np.zeros((mpc['n_b'], mpc['n_g']))
-        for gen in range(mpc['n_g']):
-            mpc['bus_gen'][int(mpc['gen'][gen, 0] - 1), gen] = 1
-
-        # Bus-uncertainty index matrix
-        mpc['bus_u'] = np.zeros((mpc['n_b'], mpc['n_u']))
-        for u in range(mpc['n_u']):
-            mpc['bus_u'][mpc['bus_uncertain'][u] - 1, u] = mpc['times_uncertain'][u]
-            
-        # Daily fixed load
-        demand_t = np.array([0.79, 0.73, 0.76, 0.81, 0.90, 0.96,
-                             0.95, 0.89, 0.88, 0.83, 0.87, 1.12,
-                             1.34, 1.52, 1.56, 1.53, 1.39, 1.02,
-                             0.79, 0.71, 0.66, 0.63, 0.60, 0.57]).reshape((-1, 1))
-        demand_t = demand_t / demand_t.max()
-        mpc['PD'] = demand_t @ mpc['bus'][:, 2].reshape((1, -1)) / mpc['baseMVA'] # Active load power
-        mpc['QD'] = demand_t @ mpc['bus'][:, 3].reshape((1, -1)) / mpc['baseMVA'] # Reactive load power
-
-        mpc['R'] = mpc['branch'][:, 2] # Resistance
-        mpc['X'] = mpc['branch'][:, 3] # Reactance
-        mpc['S'] = mpc['branch'][:, 5] / mpc['baseMVA'] # Capacity
-
-        mpc['Vu'] = mpc['bus'][:, 11] # Upper bound of voltage
-        mpc['Vl'] = mpc['bus'][:, 12] # Lower bound of voltage
-
-        mpc['Ramp'] = mpc['gen'][:, 21] / mpc['baseMVA'] # Ramp bound
-        mpc['Pmax'] = mpc['gen'][:, 8] / mpc['baseMVA'] # Maximum active generation
-        mpc['Pmin'] = mpc['gen'][:, 9] / mpc['baseMVA'] # Minimum active generation
-        mpc['Qmax'] = mpc['gen'][:, 3] / mpc['baseMVA'] # Maximum reactive generation
-        mpc['Qmin'] = mpc['gen'][:, 4] / mpc['baseMVA'] # Minimum reactive generation
-
-        # Power transfer distribution factor
-        mpc['PTDF'] = np.zeros((mpc['n_l'] + mpc['n_b'] - 1, mpc['n_b']))
-        Arel = mpc['bus_branch'][1:, :].T
-        Mrel1 = np.concatenate((Arel.T, np.zeros((mpc['n_b'] - 1, mpc['n_b'] - 1))), axis=1)
-        Mrel2 = np.concatenate((np.diag(mpc['X']), Arel), axis=1)
-        Mrel = np.concatenate((Mrel1, Mrel2), axis=0)
-        for i in range(1, mpc['n_b']):
-            erel = np.zeros((mpc['n_l'] + mpc['n_b'] - 1,))
-            erel[i - 1] = 1
-            mpc['PTDF'][:, i] = np.linalg.solve(Mrel, erel)
-        mpc['PTDF'] = mpc['PTDF'][:mpc['n_l'], :]
-        mpc['PTDF'][np.abs(mpc['PTDF']) < 1e-8] = 0
-
-        return mpc
     
     @staticmethod
     def case_ieee30():
@@ -213,72 +140,6 @@ class Case(object):
         return mpc
 
     @staticmethod
-    def test_u(su, mpc, b_print=False, b_ellipsoid=True):
-        """
-        Test whether u is in the ellipsoid uncertainty set
-        """
-        # bounds of uncertainty
-        if not np.all(su >= mpc['u_ll']):
-            if b_print:
-                print('!!!!!!!!!!!Lower bound of uncertainty does not hold!!!!!!!!!!!')
-                print('The largest violation is: {}'.format(np.max(mpc['u_ll'] - su)))
-            return False
-        if not np.all(su <= mpc['u_lu']):
-            if b_print:
-                print('!!!!!!!!!!!Upper bound of uncertainty does not hold!!!!!!!!!!!')
-                print('The largest violation is: {}'.format(np.max(su - mpc['u_lu'])))
-            return False
-        # bounds of error
-        error = mpc['u_l_predict'] - su
-        if not np.all(error >= mpc['error_lb']):
-            if b_print:
-                print('!!!!!!!!!!!!!Lower bound of error does not hold!!!!!!!!!!!!!!')
-                print('The largest violation is: {}'.format(np.max(mpc['error_lb'] - error)))
-            return False
-        if not np.all(error <= mpc['error_ub']):
-            if b_print:
-                print('!!!!!!!!!!!!!Upper bound of error does not hold!!!!!!!!!!!!!!')
-                print('The largest violation is: {}'.format(np.max(error - mpc['error_ub'])))
-            return False
-        # quadratic
-        if b_ellipsoid:
-            u_ld = error - mpc['error_mu'] # error - mu
-            if u_ld @ mpc['error_sigma_inv'] @ u_ld > mpc['error_rho']:
-                if b_print:
-                    print('!!!!!!!!!!!!!Quadratic constraint does not hold!!!!!!!!!!!!!!')
-                    print('The violation is: {}'.format(u_ld @ mpc['error_sigma_inv'] @ u_ld - mpc['error_rho']))
-                return False
-
-        return True
-    
-    @staticmethod
-    def revise_u(su, mpc, EPS=1e-8, b_print=False, b_ellipsoid=True):
-        """
-        Revise a solution that is not in the ellipsoid uncertainty set to be in the uncertainty set
-        """
-        print('Revise uncertainty.')
-        # bounds of uncertainty
-        su = np.minimum(su, mpc['u_lu'] - EPS * EPS)
-        su = np.maximum(su, mpc['u_ll'] + EPS * EPS)
-        # bounds of error
-        error = mpc['u_l_predict'] - su
-        error = np.minimum(error, mpc['error_ub'] - EPS * EPS)
-        error = np.maximum(error, mpc['error_lb'] + EPS * EPS)
-        su = mpc['u_l_predict'] - error
-        # quadratic
-        if b_ellipsoid:
-            u_ld = mpc['u_l_predict'] - su - mpc['error_mu']
-            quadratic = (1 + EPS) * u_ld @ mpc['error_sigma_inv'] @ u_ld
-            if quadratic > mpc['error_rho']:
-                u_ld = u_ld / np.sqrt(quadratic / mpc['error_rho'])
-                su = mpc['u_l_predict'] - u_ld - mpc['error_mu']
-        # Test
-        if b_print:
-            print('test_u in revise_u')
-            Case().test_u(su, mpc, b_print=True, b_ellipsoid=b_ellipsoid)
-        return su
-    
-    @staticmethod
     def case118():
         """
         118-bus case
@@ -384,4 +245,143 @@ class Case(object):
         mpc['u_ll'] = np.zeros(mpc['u_l_predict'].shape)
 
         return mpc
+    
+    @staticmethod
+    def process_case(mpc):
+        """
+        Fetch and calculate some parameters
+        """
+        mpc['branch'] = mpc['branch'][mpc['branch'][:, 10] == 1] # Branches that are on
+
+        mpc['n_b'] = mpc['bus'].shape[0] # Number of buses
+        mpc['n_l'] = mpc['branch'].shape[0] # Number of branches
+        mpc['n_g'] = mpc['gen'].shape[0] # Number of generators
+
+        mpc['c_x_pg'] = mpc['gencost'][:, 5] * mpc['baseMVA'] # Day-ahead power cost coefficient
+        mpc['c_x_rp'] = mpc['gencost'][:, 7] * mpc['baseMVA'] # Up reserve cost coefficient
+        mpc['c_x_rn'] = mpc['gencost'][:, 8] * mpc['baseMVA'] # Down reserve cost coefficient
+        mpc['c_x_og'] = np.zeros((mpc['n_g'],)) # On cost
+        mpc['c_x_ou'] = mpc['gencost'][:, 1] # Turn on cost
+        mpc['c_x_od'] = mpc['gencost'][:, 2] # Shut down cost
+        mpc['c_y_rp'] = mpc['gencost'][:, 9] * mpc['baseMVA'] # Real-time up cost coefficient
+        mpc['c_y_rn'] = mpc['gencost'][:, 10] * mpc['baseMVA'] # Real-time down cost coefficient
+
+        # Bus-branch index matrix
+        mpc['bus_branch'] = np.zeros((mpc['n_b'], mpc['n_l'])) # 1: flow out; -1: flow in
+        for branch in range(mpc['n_l']):
+            mpc['bus_branch'][int(mpc['branch'][branch, 0] - 1), branch] = 1
+            mpc['bus_branch'][int(mpc['branch'][branch, 1] - 1), branch] = -1
+
+        # Bus-generator index matrix
+        mpc['bus_gen'] = np.zeros((mpc['n_b'], mpc['n_g']))
+        for gen in range(mpc['n_g']):
+            mpc['bus_gen'][int(mpc['gen'][gen, 0] - 1), gen] = 1
+
+        # Bus-uncertainty index matrix
+        mpc['bus_u'] = np.zeros((mpc['n_b'], mpc['n_u']))
+        for u in range(mpc['n_u']):
+            mpc['bus_u'][mpc['bus_uncertain'][u] - 1, u] = mpc['times_uncertain'][u]
+            
+        # Daily fixed load
+        demand_t = np.array([0.79, 0.73, 0.76, 0.81, 0.90, 0.96,
+                             0.95, 0.89, 0.88, 0.83, 0.87, 1.12,
+                             1.34, 1.52, 1.56, 1.53, 1.39, 1.02,
+                             0.79, 0.71, 0.66, 0.63, 0.60, 0.57]).reshape((-1, 1))
+        demand_t = demand_t / demand_t.max()
+        mpc['PD'] = demand_t @ mpc['bus'][:, 2].reshape((1, -1)) / mpc['baseMVA'] # Active load power
+        mpc['QD'] = demand_t @ mpc['bus'][:, 3].reshape((1, -1)) / mpc['baseMVA'] # Reactive load power
+
+        mpc['R'] = mpc['branch'][:, 2] # Resistance
+        mpc['X'] = mpc['branch'][:, 3] # Reactance
+        mpc['S'] = mpc['branch'][:, 5] / mpc['baseMVA'] # Capacity
+
+        mpc['Vu'] = mpc['bus'][:, 11] # Upper bound of voltage
+        mpc['Vl'] = mpc['bus'][:, 12] # Lower bound of voltage
+
+        mpc['Ramp'] = mpc['gen'][:, 21] / mpc['baseMVA'] # Ramp bound
+        mpc['Pmax'] = mpc['gen'][:, 8] / mpc['baseMVA'] # Maximum active generation
+        mpc['Pmin'] = mpc['gen'][:, 9] / mpc['baseMVA'] # Minimum active generation
+        mpc['Qmax'] = mpc['gen'][:, 3] / mpc['baseMVA'] # Maximum reactive generation
+        mpc['Qmin'] = mpc['gen'][:, 4] / mpc['baseMVA'] # Minimum reactive generation
+
+        # Power transfer distribution factor
+        mpc['PTDF'] = np.zeros((mpc['n_l'] + mpc['n_b'] - 1, mpc['n_b']))
+        Arel = mpc['bus_branch'][1:, :].T
+        Mrel1 = np.concatenate((Arel.T, np.zeros((mpc['n_b'] - 1, mpc['n_b'] - 1))), axis=1)
+        Mrel2 = np.concatenate((np.diag(mpc['X']), Arel), axis=1)
+        Mrel = np.concatenate((Mrel1, Mrel2), axis=0)
+        for i in range(1, mpc['n_b']):
+            erel = np.zeros((mpc['n_l'] + mpc['n_b'] - 1,))
+            erel[i - 1] = 1
+            mpc['PTDF'][:, i] = np.linalg.solve(Mrel, erel)
+        mpc['PTDF'] = mpc['PTDF'][:mpc['n_l'], :]
+        mpc['PTDF'][np.abs(mpc['PTDF']) < 1e-8] = 0
+
+        return mpc
+    
+    @staticmethod
+    def test_u(su, mpc, b_print=False, b_ellipsoid=True):
+        """
+        Test whether u is in the ellipsoid uncertainty set
+        """
+        # bounds of uncertainty
+        if not np.all(su >= mpc['u_ll']):
+            if b_print:
+                print('!!!!!!!!!!!Lower bound of uncertainty does not hold!!!!!!!!!!!')
+                print('The largest violation is: {}'.format(np.max(mpc['u_ll'] - su)))
+            return False
+        if not np.all(su <= mpc['u_lu']):
+            if b_print:
+                print('!!!!!!!!!!!Upper bound of uncertainty does not hold!!!!!!!!!!!')
+                print('The largest violation is: {}'.format(np.max(su - mpc['u_lu'])))
+            return False
+        # bounds of error
+        error = mpc['u_l_predict'] - su
+        if not np.all(error >= mpc['error_lb']):
+            if b_print:
+                print('!!!!!!!!!!!!!Lower bound of error does not hold!!!!!!!!!!!!!!')
+                print('The largest violation is: {}'.format(np.max(mpc['error_lb'] - error)))
+            return False
+        if not np.all(error <= mpc['error_ub']):
+            if b_print:
+                print('!!!!!!!!!!!!!Upper bound of error does not hold!!!!!!!!!!!!!!')
+                print('The largest violation is: {}'.format(np.max(error - mpc['error_ub'])))
+            return False
+        # quadratic
+        if b_ellipsoid:
+            u_ld = error - mpc['error_mu'] # error - mu
+            if u_ld @ mpc['error_sigma_inv'] @ u_ld > mpc['error_rho']:
+                if b_print:
+                    print('!!!!!!!!!!!!!Quadratic constraint does not hold!!!!!!!!!!!!!!')
+                    print('The violation is: {}'.format(u_ld @ mpc['error_sigma_inv'] @ u_ld - mpc['error_rho']))
+                return False
+
+        return True
+    
+    @staticmethod
+    def revise_u(su, mpc, EPS=1e-8, b_print=False, b_ellipsoid=True):
+        """
+        Revise a solution that is not in the ellipsoid uncertainty set to be in the uncertainty set
+        """
+        print('Revise uncertainty.')
+        # bounds of uncertainty
+        su = np.minimum(su, mpc['u_lu'] - EPS * EPS)
+        su = np.maximum(su, mpc['u_ll'] + EPS * EPS)
+        # bounds of error
+        error = mpc['u_l_predict'] - su
+        error = np.minimum(error, mpc['error_ub'] - EPS * EPS)
+        error = np.maximum(error, mpc['error_lb'] + EPS * EPS)
+        su = mpc['u_l_predict'] - error
+        # quadratic
+        if b_ellipsoid:
+            u_ld = mpc['u_l_predict'] - su - mpc['error_mu']
+            quadratic = (1 + EPS) * u_ld @ mpc['error_sigma_inv'] @ u_ld
+            if quadratic > mpc['error_rho']:
+                u_ld = u_ld / np.sqrt(quadratic / mpc['error_rho'])
+                su = mpc['u_l_predict'] - u_ld - mpc['error_mu']
+        # Test
+        if b_print:
+            print('test_u in revise_u')
+            Case().test_u(su, mpc, b_print=True, b_ellipsoid=b_ellipsoid)
+        return su
     
