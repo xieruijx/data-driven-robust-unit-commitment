66770775e9c25fc5a573879f306acc6899df7e7c
Rui Xie <xie_rui1996@163.com>
2024-06-13 15:25:22 +0800

    Method comparison for test set

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # Data-Driven Robust Unit Commitment: An Integrated Forecasting and Optimization Approach
 
-## Project structure
-
-This repsoitory consists of the following parts:
-
-- **data**: Here are all data.
-  - **raw**: Here are raw data of the power system cases.
-  - **processed**: Here are all processed data, including the forecasting data.
-- **src** folder: Here all source codes.
-  - **utils**: Here are helper classes.
-- **results**: Here are all results.
-  - **strategies**: Here are all optimized robust unit commitment strategies.
-  - **outputs**: Here are all output results, including tables and figures.
-
-## Requirements
-
-This project uses:
-
-- **gurobipy**:
+The case settings are in ./src/utils/case.py
--- src/c054_method_comparison.py
+++ src/c054_method_comparison.py
@@ -8,7 +8,7 @@ from utils.io import IO
 optimization = Optimization()
 
 ## Settings
-index_u_l_predict = 0
+index_u_l_predict_set = range(18, 100)
 type_u_l = 'test'
 
 parameter = Case().case_ieee30_parameter()
@@ -18,29 +18,30 @@ parameter_epsilon0 = Case().case_ieee30_parameter(epsilon=0)
 weight_optimize = np.loadtxt('./data/processed/combination/d032_weight.txt')
 weight_error = np.loadtxt('./data/processed/combination/d032_weight.txt')
 
-## P1: The data-driven RO method using the uncertainty set without reconstruction
-validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n2', None, index_u_l_predict, 'case_ieee30', type_u_l)
-IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P1', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
+for index_u_l_predict in index_u_l_predict_set:
+    ## P1: The data-driven RO method using the uncertainty set without reconstruction
+    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n2', None, index_u_l_predict, 'case_ieee30', type_u_l)
+    IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P1', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
 
-## P2: The data-driven RO method using the weight optimized by minimizing the error measure
-validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_error, 'n1', None, index_u_l_predict, 'case_ieee30', type_u_l)
-IO().output_UC(30, index_u_l_predict, type_u_l, weight_error, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P2', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
+    ## P2: The data-driven RO method using the weight optimized by minimizing the error measure
+    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_error, 'n1', None, index_u_l_predict, 'case_ieee30', type_u_l)
+    IO().output_UC(30, index_u_l_predict, type_u_l, weight_error, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P2', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
 
-## Proposed
-validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n1', None, index_u_l_predict, 'case_ieee30', type_u_l)
-IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'Proposed', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
+    ## Proposed
+    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n1', None, index_u_l_predict, 'case_ieee30', type_u_l)
+    IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'Proposed', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
 
-## RO_max: The data-driven RO method using the 100% ellipsoidal uncertainty set
-validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n_m', None, index_u_l_predict, 'case_ieee30', type_u_l)
-IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'RO_max', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
+    ## RO_max: The data-driven RO method using the 100% ellipsoidal uncertainty set
+    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n_m', None, index_u_l_predict, 'case_ieee30', type_u_l)
+    IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'RO_max', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
 
-## RO_quantile: The data-driven RO method using the 1 - epsilon ellipsoidal uncertainty set
-validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n_q', None, index_u_l_predict, 'case_ieee30', type_u_l)
-IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'RO_quantile', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
+    ## RO_quantile: The data-driven RO method using the 1 - epsilon ellipsoidal uncertainty set
+    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n_q', None, index_u_l_predict, 'case_ieee30', type_u_l)
+    IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'RO_quantile', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
 
-## SP_approx: The data-driven SP method using approximate method to solve
-validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n1', 'approx', index_u_l_predict, 'case_ieee30', type_u_l)
-IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'SP_approx', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
+    ## SP_approx: The data-driven SP method using approximate method to solve
+    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n1', 'approx', index_u_l_predict, 'case_ieee30', type_u_l)
+    IO().output_UC(30, index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'SP_approx', folder_outputs='./results/outputs/', folder_strategies='./results/strategies/')
 
-## Organize and compare
-IO().organize_methods(30, index_u_l_predict, type_u_l, 0.05, folder_outputs='./results/outputs/')
+    ## Organize and compare
+    IO().organize_methods(30, index_u_l_predict, type_u_l, 0.05, folder_outputs='./results/outputs/')
