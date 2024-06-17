e9cb99ff85ee59cf5f95220e16b0d1058f4f3354
Rui Xie <xie_rui1996@163.com>
2024-06-17 16:56:22 +0800

    Proposed method in method comparison in 118-bus case

--- src/c064_method_comparison_118.py
+++ src/c064_method_comparison_118.py
@@ -15,30 +15,29 @@ parameter = Case().case118_parameter()
 parameter_epsilon0 = Case().case118_parameter(epsilon=0)
 parameter_smallMaxIter = Case().case118_parameter(MaxIter=20)
 
-# weight_optimize = np.loadtxt('./data/processed/weight/index_9_weight_56.txt')
-weight_optimize = np.loadtxt('./data/processed/combination/d032_weight.txt')
+weight_optimize = np.loadtxt('./data/processed/weight118/strategies/n6_weight_test0Proposed.txt')
 weight_error = np.loadtxt('./data/processed/combination/d032_weight.txt')
 
 for index_u_l_predict in index_u_l_predict_set:
-    ## P2: The data-driven RO method using the weight optimized by minimizing the error measure
-    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_error, 'n1', None, index_u_l_predict, 'case118', type_u_l)
-    IO().output_UC(index_u_l_predict, type_u_l, weight_error, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P2', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
+    # ## P2: The data-driven RO method using the weight optimized by minimizing the error measure
+    # validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_error, 'n1', None, index_u_l_predict, 'case118', type_u_l)
+    # IO().output_UC(index_u_l_predict, type_u_l, weight_error, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P2', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
 
-    ## RO_max: The data-driven RO method using the 100% ellipsoidal uncertainty set
-    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n_m', None, index_u_l_predict, 'case118', type_u_l)
-    IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'RO_max', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
+    # ## RO_max: The data-driven RO method using the 100% ellipsoidal uncertainty set
+    # validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n_m', None, index_u_l_predict, 'case118', type_u_l)
+    # IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'RO_max', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
     
-    ## P1: The data-driven RO method using the uncertainty set without reconstruction
-    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n2', None, index_u_l_predict, 'case118', type_u_l)
-    IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P1', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
+    # ## P1: The data-driven RO method using the uncertainty set without reconstruction
+    # validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n2', None, index_u_l_predict, 'case118', type_u_l)
+    # IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'P1', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
 
     ## Proposed
     validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter, weight_optimize, 'n1', None, index_u_l_predict, 'case118', type_u_l)
     IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'Proposed', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
 
-    ## SP_approx: The data-driven SP method using approximate method to solve
-    validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter_smallMaxIter, weight_optimize, 'n1', 'approx', index_u_l_predict, 'case118', type_u_l)
-    IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'SP_approx', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
+    # ## SP_approx: The data-driven SP method using approximate method to solve
+    # validation_cost, test_cost, sxb1, sxc1, LBUB1, sxb2, sxc2, LBUB2, time, train_cost, train_order, interpret = optimization.weight2cost(parameter_smallMaxIter, weight_optimize, 'n1', 'approx', index_u_l_predict, 'case118', type_u_l)
+    # IO().output_UC(index_u_l_predict, type_u_l, weight_optimize, train_cost, train_order, validation_cost, test_cost, LBUB1, LBUB2, time, interpret, 'SP_approx', folder_outputs='./results/outputs/118/', folder_strategies='./results/strategies/118/')
 
     ## Organize and compare
     IO().organize_methods(index_u_l_predict, type_u_l, 0.05, folder_outputs='./results/outputs/118/')
